2d9159ceeb8a26aff5ba38e2b63fde1544cddcb0
Martijn Kunstman <martijn@kunstman.nl>
2024-05-29 21:45:21 +0200

    wedges

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
-
-const worldSize = 800;
+const worldSize = 500;
 const foodCount = 50;
 const poisonCount = 50;
 const playerRadius = 25;
 const foodPoisonRadius = 3;
-const sensors = 5;
-const sensorsLenght = 50;
+const sensors = 8;
+const sensorsLenght = 100;
 
 let points = 0;
 let foodArray = [];
 let poisonArray = [];
 
-let player = new Player(worldSize / 2, worldSize / 2, playerRadius, sensors);
+let player = new Player(worldSize / 2, worldSize / 2, playerRadius, sensors, sensorsLenght);
 
 let controls = new Controls();
 
@@ -19,15 +18,37 @@ function calculateDistance(x1, y1, x2, y2) {
   return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
 }
 
-function createFoodPoison() {
+function calculateAngle(x1,y1,x2,y2)
+{
+  return Math.atan2(y2 - y1, x2 - x1);
+}
 
+function calculatePointFromStartPointLengthAndAngle(x1, y1, angleInRadians, length) {
+  // Calculate the new point
+  var x2 = x1 + length * Math.cos(angleInRadians);
+  var y2 = y1 + length * Math.sin(angleInRadians);
+  return { x: x2, y: y2 };
+}
+
+function createNewFood(index, array) {
+  let x = Math.random() * worldSize;
+  let y = Math.random() * worldSize;
+  if (calculateDistance(player.x, player.y, x, y) < sensorsLenght) {
+    createNewFood(index, array);
+  } else {
+    array[index].x = x;
+    array[index].y = y;
+  }
+}
+
+function createFoodPoison() {
   for (let i = 0; i < foodCount; i++) {
     let x = Math.random() * worldSize;
     let y = Math.random() * worldSize;
-   //not create food or poison inside Player
+    //not create food or poison inside Player
     if (
       calculateDistance(worldSize / 2, worldSize / 2, x, y) <
-      playerRadius * 2
+      sensorsLenght/2+playerRadius/2
     ) {
       i--;
       continue;
@@ -40,7 +61,7 @@ function createFoodPoison() {
     let y = Math.random() * worldSize;
     if (
       calculateDistance(worldSize / 2, worldSize / 2, x, y) <
-      playerRadius * 4
+      sensorsLenght/2+playerRadius/2
     ) {
       i--;
       continue;
@@ -115,6 +136,7 @@ function animate() {
   if (controls.right) {
     player.x += 1;
   }
+  pointsDiv.innerHTML = "Points: " + player.points;
   requestAnimationFrame(animate);
 }
 
--- player.js
+++ player.js
@@ -1,5 +1,5 @@
 class Player {
-  constructor(x, y, radius, sensors) {
+  constructor(x, y, radius, sensors, sensorsLenght) {
     this.x = x;
     this.y = y;
     this.radius = radius;
@@ -9,44 +9,148 @@ class Player {
     this.speed = 0;
     this.maxSpeed = 2;
     this.points = 0;
+    this.wedges = [];
+    this.sensorsLenght = sensorsLenght;
+    this.createWedges();
   }
 
-  drawWedge(centerX, centerY, radius, startAngle, endAngle, color) {
+  createWedges() {
+    for (let i = 0; i < this.sensors; i++) {
+      const startAngle = (i * 2 * Math.PI) / this.sensors - Math.PI;
+      const endAngle = ((i + 1) * 2 * Math.PI) / this.sensors - Math.PI;
+      const addAngle = (endAngle - startAngle) / 2;
+      const color = `hsl(${(i * 360) / this.sensors}, 100%, 50%)`; // Generate different colors for each wedge
+
+      this.wedges.push({
+        startAngle: startAngle + addAngle,
+        endAngle: endAngle + addAngle,
+        color: color,
+        food: 0,
+        poinson: 0,
+      });
+    }
+
+    this.wedges[this.sensors - 1].endAngle = this.wedges[0].startAngle;
+
+    console.log(this.wedges);
+  }
+
+  drawWedge(
+    centerX,
+    centerY,
+    radius,
+    startAngle,
+    endAngle,
+    color,
+    food,
+    poison
+  ) {
     ctx.beginPath();
     ctx.moveTo(centerX, centerY);
     ctx.arc(centerX, centerY, radius, startAngle, endAngle);
     ctx.closePath();
     ctx.fillStyle = color;
     ctx.fill();
-    ctx.strokeStyle = "white";
+    ctx.strokeStyle = "rgba(0,0,0,0.5)";
     ctx.stroke();
+    ctx.fillStyle = "black";
+    ctx.font = "12px Arial";
+    let point = calculatePointFromStartPointLengthAndAngle(
+      centerX,
+      centerY,
+      startAngle + Math.PI / this.sensors,
+      this.sensorsLenght / 2
+    );
+    ctx.fillText(food, point.x, point.y);
+  }
+
+  calculateWedgeValues() {
+    for (let i = 0; i < this.wedges.length; i++) {
+      this.wedges[i].food = 0;
+      this.wedges[i].poinson = 0;
+    }
+
+    for (let i = 0; i < foodArray.length; i++) {
+      if (
+        calculateDistance(player.x, player.y, foodArray[i].x, foodArray[i].y) <
+        this.sensorsLenght
+      ) {
+        //check the angle to this point
+        let angle = calculateAngle(
+          player.x,
+          player.y,
+          foodArray[i].x,
+          foodArray[i].y
+        );
+        //console.log(angle);
+
+        for (let i = 0; i < this.wedges.length; i++) {
+          //this.wedges[i].food++;
+          if (i == this.wedges.length - 1) {
+            if (
+              (angle > 0 && angle > this.wedges[i].startAngle) ||
+              (angle < 0 && angle <= this.wedges[i].endAngle)
+            ) {
+              this.wedges[i].food++;
+            }
+          } else {
+            if (
+              angle > this.wedges[i].startAngle &&
+              angle <= this.wedges[i].endAngle
+            ) {
+              this.wedges[i].food++;
+            }
+          }
+        }
+      }
+    }
   }
 
   update() {
     //check collision with food or poison
     for (let i = 0; i < foodArray.length; i++) {
-
-      if (calculateDistance(foodArray[i].x, foodArray[i].y, player.x, player.y)<this.radius)
-        {
-          foodArray[i].x=0;
-          foodArray[i].y=0;
-          this.points++;
-          console.log(this.points)
-        }
-
+      if (
+        calculateDistance(foodArray[i].x, foodArray[i].y, player.x, player.y) <
+        this.radius
+      ) {
+        createNewFood(i, foodArray);
+        this.points++;
+        //console.log(this.points);
+      }
     }
+
+    for (let i = 0; i < poisonArray.length; i++) {
+      if (
+        calculateDistance(
+          poisonArray[i].x,
+          poisonArray[i].y,
+          player.x,
+          player.y
+        ) < this.radius
+      ) {
+        createNewFood(i, poisonArray);
+        this.points--;
+        //console.log(this.points);
+      }
+    }
+    this.calculateWedgeValues();
   }
 
   draw() {
     this.update();
     //draw sensors
     // Loop to draw n wedges
-    let n = this.sensors;
-    for (let i = 0; i < n; i++) {
-      const startAngle = (i * 2 * Math.PI) / n;
-      const endAngle = ((i + 1) * 2 * Math.PI) / n;
-      const color = `hsl(${(i * 360) / n}, 100%, 50%)`; // Generate different colors for each wedge
-      this.drawWedge(player.x, player.y, 100, startAngle, endAngle, color);
+    for (let i = 0; i < this.wedges.length; i++) {
+      this.drawWedge(
+        player.x,
+        player.y,
+        this.sensorsLenght,
+        this.wedges[i].startAngle,
+        this.wedges[i].endAngle,
+        this.wedges[i].color,
+        this.wedges[i].food,
+        this.wedges[i].poison
+      );
     }
     ctx.beginPath();
     ctx.arc(player.x, player.y, player.radius, 0, Math.PI * 2);
